038d1987d8b5e7a131c2c19648194dd1706a39fd
akameco <akameco.t@gmail.com>
2018-06-19 19:29:34 +0900

    docs(readme): udpate docs

--- cli.js
+++ cli.js
@@ -10,6 +10,8 @@ const cli = meow(`
 
 	Examples
 	  $ symphogear さな.mp3
+
+	  $ symphogear http://sanabutton.ojaru.jp/sound/%E3%81%9B%E3%82%93%E3%81%9B%E3%81%88%E3%81%A8%E3%81%AA%E3%81%84%E3%81%97%E3%82%87%E3%81%B0%E3%81%AA%E3%81%97/%E3%81%A4%E3%81%8E%E3%81%A4%E3%81%8E%E3%81%A4%E3%83%BC%E3%81%8E06.mp3
 `)
 
 symphogear(cli.input[0])
