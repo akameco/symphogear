9cde85242010a9ce79a8b008860cc306166257f5
akameco <akameco.t@gmail.com>
2018-06-19 18:39:46 +0900

    feat: support url && cache

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // @flow
 'use strict'
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
 const execa = require('execa')
+const got = require('got')
+const isUrl = require('is-url-superb')
+const findCacheDir = require('find-cache-dir')
 const findExec = require('find-exec')
 
 const PLAYERS = [
@@ -18,11 +24,38 @@ function findPlayer(player /*: ?string */) {
   if (player && PLAYERS.includes(player)) {
     return player
   }
-  const execPlayer = findExec(PLAYERS)
+  const execPlayer /*: string | null */ = findExec(PLAYERS)
   if (execPlayer === null) {
     throw new Error("Couldn't find a suitable audio playe")
   }
   return execPlayer
+}
+
+function toHash(file /*: string */) {
+  const shasum = crypto.createHash('sha1')
+  shasum.update(file)
+  return shasum.digest('hex')
+}
+
+async function fetchFile(file /*: string */) {
+  if (!isUrl(file)) {
+    return file
+  }
+
+  const filePath = path.join(
+    findCacheDir({ name: 'symphogear', create: true }),
+    toHash(file)
+  )
+
+  if (fs.existsSync(filePath)) {
+    return filePath
+  }
+
+  const { body } = await got(file, { encoding: null })
+
+  fs.writeFileSync(filePath, body)
+
+  return filePath
 }
 
 const symphogear = async (
@@ -32,9 +65,8 @@ const symphogear = async (
   if (typeof file !== 'string') {
     throw new TypeError(`Expected a string, got ${typeof file}`)
   }
-
   const player = findPlayer(opts.player)
-  const result = await execa(player, [file])
+  const result = await execa(player, [await fetchFile(file)])
   return result
 }
 
